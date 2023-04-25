c506c8d45d84ac7ffbf71b634b73f1be6641dea7
ShireenAzad <syed.shireen@everest.engineering>
2023-04-25 22:31:13 +0530

    husky commit

--- src/sum.js
+++ src/sum.js
@@ -5,7 +5,6 @@ const quotient = (a, b) => a / b;
 
 const modulo = (a, b) => a % b;
 
-
 const power = (a, b) => a ** b;
 const log = (a, b) => a * Math.log(b);
 
@@ -16,33 +15,14 @@ const sqrt = (a, b) => a * Math.sqrt(b);
 const cbrt = (a, b) => a * Math.cbrt(b);
 
 export {
-  sum,
-  difference,
-
-
-
-
-
-
-
-
-
-  
-
-
-
-
-
-
-
-
-
-  product,
-  quotient,
-  modulo,
-  power,
-  log,
-  exp,
-  sqrt,
-  cbrt,
+	sum,
+	difference,
+	product,
+	quotient,
+	modulo,
+	power,
+	log,
+	exp,
+	sqrt,
+	cbrt
 };
