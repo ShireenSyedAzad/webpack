452bd0151a5d11d3497957c6854ced937562d773
ShireenAzad <syed.shireen@everest.engineering>
2023-04-25 21:47:01 +0530

    husky commit

---
+++ src/image_viewer.js
@@ -0,0 +1,5 @@
+import "../styles/image_viewer.css";
+const image = document.createElement("img");
+image.src =
+  "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png";
+document.body.appendChild(image);
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import { difference, product, sum, modulo } from "./sum";
+import "./image_viewer";
+const total = sum(1, 2);
+const diff = difference(1, 2);
+const prod = product(1, 2);
+const mod = modulo(1, 2);
+console.log(total);
+console.log(diff);
+console.log(prod);
+console.log(mod);
