dbb937a7659123b7129878b4c406cef4488b896a
ShireenAzad <syed.shireen@everest.engineering>
2023-04-25 21:10:51 +0530

    dev dependenices and husky added.

---
+++ src/sum.js
@@ -0,0 +1,34 @@
+const sum = (a, b) => a + b;
+const difference = (a, b) => a - b;
+const product = (a, b) => a * b;
+const quotient = (a, b) => a / b;
+
+const modulo = (a, b) => a % b;
+
+const power = (a, b) => a ** b;
+const log = (a, b) => a * Math.log(b);
+
+const exp = (a, b) => a * Math.exp(b);
+
+const sqrt = (a, b) => a * Math.sqrt(b);
+
+
+
+
+
+
+
+const cbrt = (a, b) => a * Math.cbrt(b);
+
+export {
+  sum,
+  difference,
+  product,
+  quotient,
+  modulo,
+  power,
+  log,
+  exp,
+  sqrt,
+  cbrt,
+};
